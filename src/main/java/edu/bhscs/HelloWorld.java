1095454c01c0586909312b9644db1f3938929170
janaesmuda <2002981@apps.nsd.org>
2024-09-13 18:19:57 +0000

    stuff

---
+++ src/main/java/edu/bhscs/HelloWorld.java
@@ -0,0 +1,10 @@
+class HelloWorld {
+  // all code in Java sits inside a class
+  // every java program needs a public main method 
+  // for the next while, All code you write will live inside main()
+  public static void main(String[] args) { 
+    System.out.println("Hello.");
+    System.out.println("Are you awake?");
+    System.out.println("Can you see me?");
+  }
+}
